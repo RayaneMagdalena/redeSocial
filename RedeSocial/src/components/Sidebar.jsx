ad8f139b87b53f6633c1afd6425e712abee31e24
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-12-01 16:53:51 -0300

    Sidebar component

---
+++ RedeSocial/src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import styles from "./Sidebar.module.css";
+
+import { PencilSimpleLine } from "@phosphor-icons/react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+       className={styles.cover} 
+       src="https://images.unsplash.com/photo-1476209446441-5ad72f223207?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img 
+        className={styles.avatar}
+        src="https://github.com/RayaneMagdalena.png" />
+       
+        <strong>Luana Vasconselos</strong>
+        <span>illustrator</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+            Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
