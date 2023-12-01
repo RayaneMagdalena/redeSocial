724d1f4c07dddddc72612cea526aad1c25a3acdf
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-12-01 14:18:21 -0300

    Header component

--- RedeSocial/src/App.jsx
+++ RedeSocial/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// CSS
+import "./global.css";
+import styles from "./App.module.css";
+// Components
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header />
 
-export default App
+      <div className={styles.wrapper}>
+        <Sidebar />
+      
+        <main>
+          <Post
+            author="Vanessa Oliveira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus eligendi consectetur esse  libero doloribus odit, rem fugiat quaerat vero sapiente! Iure, accusamus ullam nesciunt beatae vitae odio necessitatibus tenetur laboriosam."
+          />
+
+          <Post
+            author="Marcos Vales"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus eligendi consectetur esse  libero doloribus odit, rem fugiat quaerat vero sapiente! Iure, accusamus ullam nesciunt beatae vitae odio necessitatibus tenetur laboriosam."
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
