edb1952e58d21a3fddc58bfd0b3c33229e0d002f
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-12-03 13:41:20 -0300

    Likes in the comments

--- RedeSocial/src/components/Comment.jsx
+++ RedeSocial/src/components/Comment.jsx
@@ -2,10 +2,19 @@ import { ThumbsUp } from "@phosphor-icons/react/dist/ssr";
 import styles from "./Comment.module.css";
 import { Trash } from "@phosphor-icons/react";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+  
   function handleDeleteComment () {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment () {
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
   
   return (
@@ -34,9 +43,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-            <button>
+            <button onClick={handleLikeComment}>
                 <ThumbsUp size={24} />
-                Aplaudir <span>20</span>
+                Aplaudir <span>{likeCount}</span>
             </button>
         </footer>
       </div>
